3b90e1b2fa21d7ad632d58142fb5f6f0ba341aed
Shevi <shevids@gmail.com>
2019-08-24 17:56:07 +1000

    Completed base commands

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var keys = require('./utils/keys')
+var fs = require('fs')
 var axios = require('axios');
 var Logger = require('./utils/logger')
 var moment = require('moment')
@@ -22,26 +23,23 @@ var searchConcert = function (searchTerm) {
         url: `https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`
     })
         .then(function (response) {
-            if (response.data.length > 0) {
+            if (response.data.length === 0) {
+                return log("Looks like we didn't find any matches for that... Please try again", "fail");
+            }
+            var showsArray = []
+            log(`WE FOUND THE FOLLOWING CONCERTS FOR ${response.data[0].lineup[0].toUpperCase()}`, "success")
+            log("===============================================================================\n", "success", false)
 
-                var showsArray = []
-                log(`WE FOUND THE FOLLOWING CONCERTS FOR ${response.data[0].lineup[0].toUpperCase()}`, "success")
-                log("===============================================================================\n", "success", false)
+            response.data.forEach(function (show) {
+                showsArray.push({
+                    "Show Arena": show.venue.name,
+                    "City": show.venue.city,
+                    "Country": show.venue.country,
+                    "Show date": moment(show.datetime).format('LLLL')
 
-                response.data.forEach(function (show) {
-                    showsArray.push({
-                        "Show Arena": show.venue.name,
-                        "City": show.venue.city,
-                        "Country": show.venue.country,
-                        "Show date": moment(show.datetime).format('LLLL')
-
-                    })
                 })
-                console.table(showsArray)
-            }
-            else {
-                log("Whoops looks like that didn't work, Please try again", "fail")
-            }
+            })
+            console.table(showsArray)
 
 
         })
@@ -54,6 +52,7 @@ var searchConcert = function (searchTerm) {
 
 var searchSpotify = function (searchTerm) {
     var displayArray = []
+    log("SEARCHING SPOTIFY FOR SONGS \n", 'info')
     spotify.search(
         {
             type: 'track',
@@ -61,8 +60,14 @@ var searchSpotify = function (searchTerm) {
         }
         , function (err, data) {
             if (err) {
-                return log(`Looks like we encountered an error: ${err}`);
+                return log(`Looks like we encountered an error: ${err}`, 'fail');
             }
+
+            if (data.tracks.items === 0) {
+                return log("Looks like we didn't find any matches for that... Please try again", "fail");
+            }
+
+            log("WE FOUND THE FOLLOWING MATCHING TRACKS: \n", 'success')
 
             data.tracks.items.forEach(function (trackitem) {
                 log(`ARTIST       : ${trackitem.artists[0].name}`, 'success', false)
@@ -74,6 +79,37 @@ var searchSpotify = function (searchTerm) {
             })
 
         });
+}
+
+var movieSearch = function (searchTerm) {
+    log("FETCHING MOVIE INFO\n", 'info')
+    axios({
+        method: "GET",
+        url: `http://www.omdbapi.com/?t=${searchTerm}&apikey=trilogy&`
+    })
+        .then(function (response) {
+            var rottenTomatoesScore = ""
+
+            response.data['Ratings'].forEach(function (rating) {
+                if (rating['Source'] === "Rotten Tomatoes") {
+                    rottenTomatoesScore = rating['Value']
+                }
+            })
+
+            // response.data
+            log(`TITLE                  : ${response.data['Title']}`, 'success', false)
+            log(`YEAR                   : ${response.data['Year']}`, 'success', false)
+            log(`RATING                 : ${response.data['imdbRating']}`, 'success', false)
+            log(`ROTTEN TOMATOES RATING : ${rottenTomatoesScore}`, 'success', false)
+            log(`COUNTRY                : ${response.data['Country']}`, 'success', false)
+            log(`PLOT                   : ${response.data['Plot']}`, 'success', false)
+            log(`ACTORS                 : ${response.data['Actors']}`, 'success', false)
+
+
+        })
+        .catch(function (err) {
+            log('An error occured fetching the data', 'fail')
+        })
 }
 
 
@@ -95,3 +131,18 @@ if (search.command === "concert-this") {
 else if (search.command === "spotify-this-song") {
     searchSpotify(search.searchString)
 }
+else if (search.command === "movie-this") {
+    movieSearch(search.searchString)
+}
+else if (search.command === "do-what-it-says") {
+    fs.readFile('./utils/random.txt', function (err, data) {
+        if (err) {
+            return log("Unable to run command", 'fail')
+        }
+        searchSpotify(data.toString().split(',')[1])
+    })
+}
+else {
+    log("That command is unrecognised... Please try again", 'fail')
+}
+
